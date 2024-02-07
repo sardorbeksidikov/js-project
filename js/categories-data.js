70f284547958c51287aa1b29137d46dc06b45107
sardorbeksidikov <sardorsidikov005@gmail.com>
2024-02-07 08:11:29 +0500

    sardorbek

--- js/categories-data.js
+++ js/categories-data.js
@@ -1,32 +1,70 @@
 const categories = [
-  { id: 1, name: "Fruit", image: "https://picsum.photos/200/300/?category0" },
-  { id: 2, name: "Bakery", image: "https://picsum.photos/200/300/?category2" },
+  { id: 1, name: "Fruit", image: "../assets/images/catigoies/1.png" },
+  { id: 2, name: "Bakery", image: "../assets/images/catigoies/2.png" },
   {
     id: 3,
     name: "Vegetable",
-    image: "https://picsum.photos/200/300/?category3",
+    image: "../assets/images/catigoies/3.png",
   },
-  { id: 4, name: "Dairy", image: "https://picsum.photos/200/300/?category5" },
-  { id: 5, name: "Meat", image: "https://picsum.photos/200/300/?category6" },
+  { id: 4, name: "Dairy", image: "../assets/images/catigoies/4.png" },
+  { id: 5, name: "Meat", image: "../assets/images/catigoies/5.png" },
   {
     id: 6,
     name: "Dessert",
-    image: "https://picsum.photos/200/300/?category7",
+    image: "../assets/images/catigoies/6.png",
   },
   {
     id: 7,
     name: "Fast food",
-    image: "https://picsum.photos/200/300/?category12",
+    image: "../assets/images/catigoies/7.png",
   },
   {
     id: 8,
     name: "Breakfast",
-    image: "https://picsum.photos/200/300/?category23",
+    image: "../assets/images/catigoies/8.png",
   },
-  { id: 9, name: "Lunch", image: "https://picsum.photos/200/300/?category27" },
+  { id: 9, name: "Lunch", image: "../assets/images/catigoies/9.png" },
   {
     id: 10,
-    name: "Dinner",
-    image: "https://picsum.photos/200/300/?category31",
+    name: "boy",
+    image: "../assets/images/catigoies/10.png",
   },
+  {
+    id: 10,
+    name: "bek",
+    image: "../assets/images/catigoies/11.png",
+  },
+ 
 ];
+
+const catalogy = document.querySelector(".catalogy");
+
+function displaycatalogy(categories) {
+  let str = "";
+  categories.forEach((produc) => {
+    str += `
+    <div class= "box">
+      <img class="box__img" src="${produc.image}" alt="${produc.name}">
+    </div>  
+    `;
+  });
+  catalogy.innerHTML = str;
+}
+displaycatalogy(categories);
+
+
+
+newArray = [];
+
+document.getElementById("search").addEventListener("keyup", function(){
+  let search = this.value.toLocaleLowerCase();
+  newArray = array.filter(function(val){
+
+    if(val.id.includes(search) || val.name.includes(search)){
+
+      let newobj = {id : val.id, name: val.name};
+      return newobj;
+    }
+  })
+  showtable(newArray)
+})
